b983cb4180ae44a67698178e4e583fd476a4183d
Gabriele Alessi <89archaon89@gmail.com>
2021-03-26 15:14:42 +0100

    Esercizio 2 completato

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ function es1() {
     // una bici da corsa con le seguenti proprietà: nome e peso.
     // Stampare a schermo la bici con peso minore.
     const bikes = [
-        {'nome': 'bici1', 'peso': 5},
-        {'nome': 'bici2','peso': 7},
-        {'nome': 'bici3','peso': 2.5}
+        {'nome': 'graziella1', 'peso': 8},
+        {'nome': 'graziella2','peso': 11},
+        {'nome': 'graziella3','peso': 5},
+        {'nome': 'graziella4','peso': 3.5},
+        {'nome': 'graziella5','peso': 6}
     ];
     let lightBike = {nome: '', peso: Number.MAX_SAFE_INTEGER};
     for (let i=0;i<bikes.length;i++) {
@@ -19,10 +21,10 @@ function es1() {
 }
 
 //genero 2 numeri casuali
-function get2Rnd(max) {
+function get2Rnd() {
     let arr = [];
     for (let i = 0; i < 2; i++) {
-        let rnd = Math.floor(Math.random()*max);
+        let rnd = Math.floor(Math.random()*20);
         arr.push(rnd);
     }
     return(arr)
@@ -36,7 +38,9 @@ function es2() {
     // le altre saranno tutte settate a 0.
     // Generare numeri random al posto degli 0 nelle proprietà:
     // punti fatti e falli subiti
-
+    // Usando la destrutturazione creiamo un nuovo array
+    // i cui elementi contengono solo nomi e falli subiti
+    // e stampiamo tutto in console.
 
     const Teams = [
         {'nome': 'gialli','punti':0, 'falli':0},
@@ -48,13 +52,19 @@ function es2() {
 
     for (let i = 0; i < Teams.length; i++) {
         const team = Teams[i];
-        let arrRnd = get2Rnd(20);
+        let arrRnd = get2Rnd();
         let [punti, falli] = arrRnd;
 
         team['punti'] = punti;
         team.falli = falli;
+
+        let newTeam = {};
+        newTeam['nome'] = team['nome'];
+        newTeam['falli'] = team['falli'];
+        newTeams.push(newTeam);
     }
     console.log(Teams);
+    console.log(newTeams);
 }
 
 
